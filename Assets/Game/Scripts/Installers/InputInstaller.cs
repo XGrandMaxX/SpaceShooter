e71c567e8077b6c6dc7a73189ca3cf4ba29c608f
GrandMax <alex.max5568@gmail.com>
2024-04-07 14:58:40 +0500

    SS-1
    --Scripts--
    1) Add ObjectPool
    2) Add PlayerMove, PlayerShoot & PlayerInputData
    3) Add Projectile
    4) Change InputInstaller

--- Assets/Game/Scripts/Installers/InputInstaller.cs
+++ Assets/Game/Scripts/Installers/InputInstaller.cs
@@ -1,15 +1,20 @@
+using Game.Scripts.Player;
 using Game.Scripts.Systems;
+using UnityEngine;
+using UnityEngine.Serialization;
 using Zenject;
 
 namespace Game.Scripts.Installers
 {
     public class InputInstaller : MonoInstaller
     {
+        [FormerlySerializedAs("_playerData")] [SerializeField] private PlayerInputData playerInputData;
         public override void InstallBindings() => Bind();
 
         private void Bind()
         {
             Container.Bind<InputController>().AsSingle().NonLazy();
+            Container.Bind<PlayerInputData>().FromInstance(playerInputData).AsSingle();
         }
     }
 }
--- Assets/Game/Scripts/Systems/InputController.cs
+++ Assets/Game/Scripts/Systems/InputController.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Game.Scripts.Systems
 {
@@ -7,12 +10,30 @@ namespace Game.Scripts.Systems
         private InputActions _inputActions;
         public InputActions InputActions => _inputActions;
         
+        private Dictionary<object, Action> _subscribers = new(5);
+        
         public InputController()
         {
             _inputActions = new InputActions();
             _inputActions.Enable();
             
-            Debug.Log("Successfully initialize!");
+            Debug.Log("InputController successfully initialize!");
+        }
+        
+        public void Subscribe(object subscriber, Action action, InputAction inputAction)
+        {
+            if (!_subscribers.TryAdd(subscriber, action)) 
+                return;
+            
+            inputAction.performed += context => action();
+        }
+
+        public void UnSubscribe(object subscriber)
+        {
+            if (!_subscribers.ContainsKey(subscriber))
+                return;
+            
+            _subscribers.Remove(subscriber);
         }
     }
 }
